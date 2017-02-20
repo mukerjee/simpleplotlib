d5deeb8def20fc239f533b059416abf11d9cc111
Matt Mukerjee <mukerjee@cs.cmu.edu>
2017-02-20 15:47:10 -0500

    adding setup info

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+REQUIREMENTS = ['matplotlib>=2.0.0', 'numpy', 'dotmap']
+
+setup(
+    name='simpleplotlib',
+    packages=['simpleplotlib'],
+    version='0.1',
+    description='A matplotlib wrapper focused on beauty and simplicity',
+    author='Matthew K. Mukerjee',
+    author_email='Matthew.Mukerjee@gmail.com',
+    url='https://github.com/mukerjee/simpleplotlib',
+    download_url='https://github.com/mukerjee/simpleplotlib/tarball/0.1',
+    license='MIT License',
+    keywords=['matplotlib', 'plots', 'beauty', 'simplicity'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Scientific/Engineering'
+    ],
+    install_requires=REQUIREMENTS
+)
