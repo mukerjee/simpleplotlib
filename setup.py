fea3c14db1e69349587ccc0be1697ab10b1f5101
Matt Mukerjee <mukerjee@cs.cmu.edu>
2017-04-14 14:45:41 -0400

    update for release 0.11

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='simpleplotlib',
     packages=['simpleplotlib'],
-    version='0.1',
+    version='0.11',
     description='A matplotlib wrapper focused on beauty and simplicity',
     author='Matthew K. Mukerjee',
     author_email='Matthew.Mukerjee@gmail.com',
     url='https://github.com/mukerjee/simpleplotlib',
-    download_url='https://github.com/mukerjee/simpleplotlib/tarball/0.1',
+    download_url='https://github.com/mukerjee/simpleplotlib/tarball/0.11',
     license='MIT License',
     keywords=['matplotlib', 'plots', 'beauty', 'simplicity'],
     classifiers=[
